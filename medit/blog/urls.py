595adf4736c9ab126354cc4b7c0289064af38b99
blacksea3 <17865191779@163.com>
2017-08-11 23:03:39 +0800

    add login.html(static)

--- medit/blog/urls.py
+++ medit/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     url(r'^$', views.test, name='test'),
     url(r'^test/$', views.test, name='test'),
+	url(r'^login/$', views.login, name='login'),
 ]
--- medit/blog/views.py
+++ medit/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from blog.models import *
 
+#from blog.user import login_verify
+
 # Create your views here.
 def test(request):
     objects = Blog.objects.all()
@@ -10,3 +12,18 @@ def test(request):
         #'status':status,
         #'logged':logged,
         })
+
+## 登陆页面		
+def login(request):
+	if request.META['REQUEST_METHOD'] == 'GET':
+		return render(request, 'blog/login.html',{
+			#'tip':tip,
+			#'status':status,
+			#'logged':logged,
+			})
+	else:
+		raise Exception('This is a POST request?!')
+	#if request.
+	
+	#result = login_verify(request)
+	

b7107d598fc62bebfd052086b4f1f57df1847de4
blacksea3 <17865191779@163.com>
2017-08-19 22:49:12 +0800

    简单地增加了article.html和blocklist.html

--- medit/main/urls.py
+++ medit/main/urls.py
@@ -1,20 +1,22 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic.base import RedirectView
 
 urlpatterns=[
-    url(r'^$', views.index, name='index'),
-	url(r'^index.html/$', views.index, name='index'),
-	#url(r'^block-add/$', views.block_add, name='block_add'),
-	#url(r'^block-add.html/$', views.block_add, name='block_add'),
-	#url(r'^block-list.html/$', views.block_list, name='block_list'),
-	#url(r'^block-edit/$', views.block_edit, name='block_edit'),
-	#url(r'^block-edit.html/$', views.block_edit, name='block_edit'),
-	#url(r'^block-del/$', views.block_del, name='block_del'),
-	#
-	#url(r'^article-add/$', views.article_add, name='article_add'),
-	#url(r'^article-add.html/$', views.article_add, name='article_add'),
-	#url(r'^article-list.html/$', views.article_list, name='article_list'),
-	#url(r'^article-edit/$', views.article_edit, name='article_edit'),
-	#url(r'^article-edit.html/$', views.article_edit, name='article_edit'),
-	#url(r'^article-del/$', views.article_del, name='article_del'),	
+    url(r'^$', RedirectView.as_view(url="index.html/")),
+    url(r'^index.html/$', views.index, name='index'),
+    url(r'^block.html/$', views.block, name='block'),
+    url(r'^article.html/$', views.article, name='article'),
+    #url(r'^block-add.html/$', views.block_add, name='block_add'),
+    #url(r'^block-list.html/$', views.block_list, name='block_list'),
+    #url(r'^block-edit/$', views.block_edit, name='block_edit'),
+    #url(r'^block-edit.html/$', views.block_edit, name='block_edit'),
+    #url(r'^block-del/$', views.block_del, name='block_del'),
+    #
+    #url(r'^article-add/$', views.article_add, name='article_add'),
+    #url(r'^article-add.html/$', views.article_add, name='article_add'),
+    #url(r'^article-list.html/$', views.article_list, name='article_list'),
+    #url(r'^article-edit/$', views.article_edit, name='article_edit'),
+    #url(r'^article-edit.html/$', views.article_edit, name='article_edit'),
+    #url(r'^article-del/$', views.article_del, name='article_del'), 
 ]
--- medit/main/views.py
+++ medit/main/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render
 ## 输出内容，格式为Queryset
 from blog.common_select import select_block_bytime
 
+## 按照ID寻找板块
+## 输入ID
+## 输出格式为Queryset    
+from blog.common_select import select_block_byid
+
 ## 按照时间由近至远寻找某版块下的文章
 ## 输入blockid,范围(若blockid=0表示全板块搜索)
 ## 输出内容，格式为Queryset
@@ -20,6 +25,11 @@ from blog.common_select import select_article_byhot
 ## 输出格式为Queryset
 from blog.common_select import select_article_bytop
 
+## 按照ID寻找文章
+## 输入ID
+## 输出格式为Queryset
+from blog.common_select import select_article_byid
+
 # Create your views here.
 
 ## 首页
@@ -28,7 +38,55 @@ def index(request):
     new_article_data = select_article_bytime(0,(0,8))   #最新文章,全板块,8条
     hot_article_data = select_article_byhot(0,(0,5))    #热门文章,全板块,5条
     
-    return_dict = {'top_article_data':top_article_data,
+    block_data = select_block_bytime((0,6))             #板块列表,6条
+    
+    return_dict = {'block_data':block_data,
+        'top_article_data':top_article_data,
         'new_article_data':new_article_data,
         'hot_article_data':hot_article_data}
     return render(request, 'main/index.html',return_dict)
+
+## 板块页
+def block(request):
+    blockid = request.GET.get('id')
+    if blockid and blockid.isdigit():
+        blockid = int(blockid)
+    else:
+        blockid = 1
+
+    new_article_data_thisblock = select_article_bytime(blockid,(0,5))    #此板块热门文章,5条
+    new_article_data = select_article_bytime(0,(0,8))   #最新文章,全板块,8条
+    hot_article_data = select_article_byhot(0,(0,5))    #热门文章,全板块,5条
+    
+    block_data = select_block_bytime((0,6))             #板块列表,6条
+    present_block_data = select_block_byid(blockid)
+    
+    return_dict = {'block_data':block_data,
+        'new_article_data_thisblock':new_article_data_thisblock,
+        'new_article_data':new_article_data,
+        'hot_article_data':hot_article_data,
+        'present_block_data':present_block_data
+        }
+    return render(request, 'main/blocklist.html',return_dict)
+    
+## 文章页
+def article(request):
+    articleid = request.GET.get('id')
+    if articleid and articleid.isdigit():
+        articleid = int(articleid)
+    else:
+        articleid = 1
+    
+    present_article_data = select_article_byid(articleid)    #此文章
+    new_article_data = select_article_bytime(0,(0,8))   #最新文章,全板块,8条
+    hot_article_data = select_article_byhot(0,(0,5))    #热门文章,全板块,5条
+    
+    block_data = select_block_bytime((0,6))             #板块列表,6条
+    
+    return_dict = {'block_data':block_data,
+        'present_article_data':present_article_data,
+        'new_article_data':new_article_data,
+        'hot_article_data':hot_article_data
+        }
+    return render(request, 'main/article.html',return_dict)
+    

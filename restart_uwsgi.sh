f7238ecb1926a926dc2f7bf183da72597aa51001
blacksea3 <17865191779@163.com>
2017-08-24 22:53:07 +0800

    服务器端修改ini,sh以及gitignore配置

--- restart_uwsgi.sh
+++ restart_uwsgi.sh
@@ -1,2 +1,21 @@
 uwsgi --stop /home/medit_uwsgi.pid
-uwsgi --ini medit_uwsgi.ini
+max_loop_time=5
+while (($max_loop_time>0))
+do
+    num_of_uwsgi=$(ps -aux|grep medit_uwsgi |wc -l)
+    if (($num_of_uwsgi>1)); then
+        echo "medit_uwsgi still occur, please wait until it stops"
+        max_loop_time=$(($max_loop_time-1))
+        sleep 1s
+    else
+        echo "medit_uwsgi has stopped"
+        break
+    fi
+done
+
+if (($max_loop_time==0));then
+    echo "medit_uwsgi hasn't stopped after I send order to it 5 seconds!, pleasecheck the status of uwsgi, I cannot start it when it is still running):"
+else
+    echo "I will start medit_uwsgi again:)"
+    uwsgi --ini medit_uwsgi.ini
+fi
